ac17e4c68ca98b323f1f4661c257e078f0506181
Your Name <you@example.com>
2022-09-17 23:24:26 +0700

    110. Tái cấu trúc và tối ưu code #1

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -12,7 +12,7 @@ import {
 import Tippy from '@tippyjs/react';
 import classNames from 'classnames/bind';
 import 'tippy.js/dist/tippy.css';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
@@ -157,7 +157,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo')}>
+                <Link to={config.routes.home} className={cx('logo')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
 
